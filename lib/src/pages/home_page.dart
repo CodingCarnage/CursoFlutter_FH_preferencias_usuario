2a1a944c2471e539da0cdb6c60e42d6f2483b82b
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2020-12-29 21:51:49 -0600

    Se creo un drawer con su componentes que a su vez direcciona a una pagina.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:preferencias_usuario/src/pages/settings_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -11,6 +12,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('User preferences'),
       ),
+      drawer: _createMenu(context),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
@@ -20,6 +22,58 @@ class HomePage extends StatelessWidget {
           const Divider(),
           const Text('Nombre usuario:'),
           const Divider(),
+        ],
+      ),
+    );
+  }
+
+  Drawer _createMenu(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Container(),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/menu-img.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.pages,
+              color: Colors.blue,
+            ),
+            title: Text('Pages'),
+            onTap: () => Navigator.pushReplacementNamed(context, HomePage.routeName),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.nightlife,
+              color: Colors.blue,
+            ),
+            title: Text('Party Mode'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.people,
+              color: Colors.blue,
+            ),
+            title: Text('People'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.settings,
+              color: Colors.blue,
+            ),
+            title: Text('Settings'),
+            onTap: () =>
+                Navigator.pushReplacementNamed(context, SettingsPage.routeName),
+          ),
         ],
       ),
     );
