afb395e993d46eeae4dd5a3a1c5312f91de27412
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2020-12-29 21:27:34 -0600

    Corrección

--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -4,6 +4,7 @@ class SettingsPage extends StatelessWidget {
   const SettingsPage({Key key}) : super(key: key);
 
   static final String routeName = 'settings';
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
